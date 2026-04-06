d1aaa8b81c283364cd971e93d1dfab0459c94083
miabarada <mia.barada2005@gmail.com>
2026-04-07 01:49:57 +0200

    add prisma module and responseInterceotir

---
+++ apps/backend/src/transform.interceptor.ts
@@ -0,0 +1,15 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { map, Observable } from "rxjs";
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+      return next.handle().pipe(
+         map((data) => ({
+            statusCode: context.switchToHttp().getResponse().statusCode,
+            message: 'Success',
+            data: data
+         })),
+      );
+   }
+}
