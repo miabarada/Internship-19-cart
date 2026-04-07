a271ca83cb8c3c72e6b004078bf49ef87ff64abd
miabarada <mia.barada2005@gmail.com>
2026-04-08 00:43:53 +0200

    add register page

--- apps/frontend/src/Router.tsx
+++ apps/frontend/src/Router.tsx
@@ -3,6 +3,7 @@ import { routes } from "./routes/routes"
 import { WelcomePage } from "./pages/WelcomePage/WelcomePage"
 import { PublicRoute } from "./routes/PublicRoute"
 import { LoginPage } from "./pages/LoginPage/LoginPage"
+import { Register } from "./pages/RegisterPage/RegisterPage"
 
 export const Router = () => {
    return (
@@ -12,6 +13,7 @@ export const Router = () => {
 
             <Route element={<PublicRoute />}>
                <Route path={routes.LOGIN_PAGE} element={<LoginPage />}/>
+               <Route path={routes.REGISTER_PAGE} element={<Register />}/>
             </Route>
          </Routes>
       </BrowserRouter>
---
+++ apps/frontend/src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import styles from './RegisterPage.module.scss'
+import { Link, useNavigate } from 'react-router-dom'
+import { routes } from '../../routes/routes'
+
+export function Register() {
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
+   const [confirmPassword, setConfirmPassword] = useState('')
+   const [error, setError] = useState('')
+   const [loading, setLoading] = useState(false)
+   const [fullName, setFullName] = useState('')
+   const navigate = useNavigate();
+
+   const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault()
+      setError('')
+
+      if (password !== confirmPassword) {
+         setError('Passwords dont match')
+         return
+      }
+
+      setLoading(true)
+
+      try {
+         const response = await fetch('http://localhost:3000/user/register', {
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password, fullName})
+         })
+
+         const data = await response.json()
+
+         if (!response.ok) {
+            throw new Error(data.message || 'Registration not successful')
+         }
+
+         localStorage.setItem('token', data.token)
+
+         navigate(routes.HOME)
+         window.location.reload()
+      } catch (err: any) {
+         setError(err.message)
+      } finally {
+         setLoading(false)
+      }
+   }
+
+   return (
+      <div className={styles.container}>
+         <h1 className={styles.title}>Register</h1>
+         {error && <p>{error}</p>}
+
+         <form onSubmit={handleSubmit} className={styles.form}>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="name@example.com" required className={styles.inputField}/>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required className={styles.inputField}/>
+            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Repeat password" required className={styles.inputField}/>
+            <input type="name" value={fullName} onChange={(e) => setFullName(e.target.value)} placeholder="Full name" required className={styles.inputField}/>
+            <button type="submit" disabled={loading} className={styles.button}> {loading ? 'Registration...' : 'Register'}</button>
+         </form>
+         <p className={styles.link}>Already have an account? <Link to={routes.LOGIN_PAGE}>Register here</Link></p>
+      </div>
+   )
+}
