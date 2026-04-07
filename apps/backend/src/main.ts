3daa1a75763ec4414be987e3219d04a3d86ef9d8
miabarada <mia.barada2005@gmail.com>
2026-04-07 09:54:20 +0200

    setup swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,15 @@ async function bootstrap() {
   app.enableCors();
   app.setGlobalPrefix('/api');
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce app API')
+    .setDescription('E-commerce API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
